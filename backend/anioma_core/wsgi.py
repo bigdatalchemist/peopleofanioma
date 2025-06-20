027132efccd1a155c1beeeea7175e6cd59823a96
FIDELIS A EZEADIEFE <fidelisezeadiefe@gmail.com>
2025-06-20 22:42:23 +0100

    Added init file to apps and improved WSGI

--- backend/anioma_core/wsgi.py
+++ backend/anioma_core/wsgi.py
@@ -8,6 +8,10 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
+from pathlib import Path
+BASE_DIR = Path(__file__).resolve().parent.parent  # points to backend/
+sys.path.append(str(BASE_DIR))
 
 from django.core.wsgi import get_wsgi_application
 
