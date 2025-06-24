e81f5240d50ea1569c8f500eabd4dd16cb3cb6be
FIDELIS A EZEADIEFE <fidelisezeadiefe@gmail.com>
2025-06-24 13:25:43 +0100

    Edited the app settings

--- backend/anioma_core/settings.py
+++ backend/anioma_core/settings.py
@@ -209,7 +209,7 @@ USE_TZ = True
 STATIC_URL = "/static/"
 MEDIA_URL = "/media/"
 STATICFILES_DIRS = [
-    BASE_DIR.parent / "static",
+    BASE_DIR / "static",
     BASE_DIR.parent / "data",  # ← Now includes your GeoJSON files
 ]
 
