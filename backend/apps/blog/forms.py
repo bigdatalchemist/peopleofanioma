82f9dc1503835354a7f8c402ab549a93b21986d1
FIDELIS A EZEADIEFE <fidelisezeadiefe@gmail.com>
2026-01-16 01:14:29 +0100

    Fixed the Blog forms

--- backend/apps/blog/forms.py
+++ backend/apps/blog/forms.py
@@ -23,7 +23,18 @@ class BlogSearchForm(forms.Form):
 class VideoPostForm(forms.ModelForm):
     class Meta:
         model = VideoPost
-        fields = ["title", "description", "category", "external_url", "thumbnail", "is_published", "is_featured", "published_at"]
+        fields = [
+            "title", 
+            "description", 
+            "category", 
+            "video_file",
+            "external_url", 
+            "thumbnail", 
+            "is_published", 
+            "is_featured", 
+            "published_at"
+            ]
+        
         widgets = {
             "published_at": forms.DateTimeInput(attrs={"type": "datetime-local"}),
         }
@@ -31,11 +42,17 @@ class VideoPostForm(forms.ModelForm):
     def clean(self):
         cleaned = super().clean()
 
-        url = cleaned.get("external_url")
+        video_url = cleaned.get("video_file")
+        external = cleaned.get("external_url")
 
-        if not url:
+
+        if not video_url and not external:
             raise forms.ValidationError(
                 "Provide a video URL (S3 or external embed)."
+            )
+        if video_url and external:
+            raise forms.ValidationError(
+                "Choose either a video URL or an external embed URL, not both."
             )
 
         return cleaned
